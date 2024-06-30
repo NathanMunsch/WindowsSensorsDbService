c6593ef12aaf83a0bf9511494c47e2e941814a4d
NathanMunsch <nathan2003munsch@gmail.com>
2024-06-30 17:32:51 +0200

    feat: Add installer for service

---
+++ WindowsSensorsDbService/Installer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+using System.Threading.Tasks;
+
+namespace WindowsSensorsDbService
+{
+    [RunInstaller(true)]
+    public partial class Installer : System.Configuration.Install.Installer
+    {
+        private ServiceProcessInstaller processInstaller;
+        private ServiceInstaller serviceInstaller;
+
+        public Installer()
+        {
+            InitializeComponent();
+
+            // Initialize ServiceProcessInstaller
+            processInstaller = new ServiceProcessInstaller();
+            processInstaller.Account = ServiceAccount.LocalSystem;
+
+            // Initialize ServiceInstaller
+            serviceInstaller = new ServiceInstaller();
+            serviceInstaller.ServiceName = "WindowsSensorsDbService";
+            serviceInstaller.StartType = ServiceStartMode.Automatic;
+
+            // Add installers to collection
+            Installers.Add(processInstaller);
+            Installers.Add(serviceInstaller);
+        }
+    }
+}
