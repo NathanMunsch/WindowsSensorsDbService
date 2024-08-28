377a644633fd6b8b9b4efb861463abbf2a239383
NathanMunsch <nathan2003munsch@gmail.com>
2024-08-28 22:30:17 +0200

    feat: Create helper to determine sensors unit

---
+++ WindowsSensorsDbService/Helpers/DetermineSensorUnit.cs
@@ -0,0 +1,20 @@
+﻿using LibreHardwareMonitor.Hardware;
+
+namespace WindowsSensorsDbService.Helpers
+{
+    public static class DetermineSensorUnit
+    {
+        public static string DetermineUnit(ISensor sensor)
+        {
+            switch (sensor.SensorType)
+            {
+                case SensorType.Temperature:
+                    return "°C";
+                case SensorType.Load:
+                    return "%";
+                default:
+                    return string.Empty;
+            }
+        }
+    }
+}
